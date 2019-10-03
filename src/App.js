6eea067ad3c95a763a03ce775aa30cbe0ac3c4c6
reddynie11 <reddy.nie11@gmail.com>
2019-10-03 18:48:06 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardList from './components/cardList/cardList.component'
+import AppNavbar from './components/navbar/navbar.component';
+
+
+class App extends React.Component{
+  
+  state ={
+    contacts: [],
+    searchTerm: ''
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(responce => responce.json())
+      .then(data => this.setState({contacts : data}))
+  }
+  handleSearch = (e)=>{
+    this.setState({searchTerm:e.target.value})
+  }
+  render(){
+    const { contacts, searchTerm } = this.state;
+    const filterContact = contacts.filter(contact => contact.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    return(
+      <div className='App'>
+        <AppNavbar handleSearch = {this.handleSearch} />
+        <CardList contacts={filterContact} />
+          
+      </div>
+      
+    )
+  }
 }
 
 export default App;
